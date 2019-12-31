00953e187e587affe04eb9751261a398cda15239
desirekaleba <desirekaleba@gmail.com>
2019-12-31 22:05:05 +0300

    next function set up

--- js/app.js
+++ js/app.js
@@ -19,9 +19,26 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
     }
 });
 
+document.querySelector('.btn-hold').addEventListener('click', function(){
+
+    scores[activePlayer] += roundScore;
+
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+});
+
 function nextPlayer(){
     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    document.querySelector('.dice').style.display = "none";
+
+
 
     
 }
